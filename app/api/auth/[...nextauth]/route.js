ad37a9d97edeccbb8a4b02d867ec1f612b20eaa2
George Acquah <georgeacquah889@gmail.com>
2023-08-05 07:24:51 +0000

    Implemented Google providers authentication

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -21,45 +21,46 @@ const handler = NextAuth({
     }),
   ], 
 
-  async session({ session }) {
-    try {
-        const sessionUser = await User.findOne({
-          email: session.user.email,
-        });
-
-        session.user.id = sessionUser._id;
-
-        return session; // Return the modified session
-    } catch (error) {
-      console.error(error);
-      return session; // Return the original session on error
-    }
-  },
-
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
-
-      //CHECK IF USER ALREADY EXISTS
-      const userExists = await User.findOne({
-        email: profile.email,
-      });
-
-      //ELSE CREATE A NEW USER AND SAVE IN DB
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(' ', '').toLowerCase(),
-          image: profile.picture,
-        });
+  callbacks: {
+    async session({ session }) {
+      try {
+          const sessionUser = await User.findOne({
+            email: session.user.email,
+          });
+          session.user.id = sessionUser._id.toString();
+  
+          return session; // Return the modified session
+      } catch (error) {
+        console.error(error);
+        return session; // Return the original session on error
       }
-
-      return true;
-    } catch (error) {
-      console.error(error);
-      return false;
-    }
-  },
+    },
+  
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+  
+        //CHECK IF USER ALREADY EXISTS
+        const userExists = await User.findOne({
+          email: profile.email,
+        });
+  
+        //ELSE CREATE A NEW USER AND SAVE IN DB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(' ', '').toLowerCase(),
+            image: profile.picture,
+          });
+        }
+  
+        return true;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    },
+  }
 });
 
 export { handler as GET, handler as POST };
