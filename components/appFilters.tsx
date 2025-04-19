5111b4f04dd1f8af2888e1a3ae98facc7eacaaf9
George Acquah <92464817+George-Acquah@users.noreply.github.com>
2025-04-19 12:35:27 +0000

    Fixed filters bug in the search bar

--- components/appFilters.tsx
+++ components/appFilters.tsx
@@ -6,6 +6,7 @@ import {
   DropdownMenuLabel,
   DropdownMenuItem,
   DropdownMenuSeparator,
+  DropdownMenuCollapsible,
 } from "./ui/dropdown";
 import { Typography } from "./ui/typography";
 import { CSSProperties  } from "react";
@@ -33,107 +34,111 @@ const sortOptions: IDropdownArray[] = [
 const AppFilters = ({
   filterStyles,
   className,
-  tags
+  tags,
 }: {
   filterStyles: CSSProperties | undefined;
-    className?: string;
-  tags: _ITrendingTags[]
-  }) => {
-  // const [tagsData, setTagsData] = useState<_ITrendingTags[]>([]);
+  className?: string;
+  tags: _ITrendingTags[];
+}) => {
   const {
     handleSetParams,
     modalValue: value,
     paramValues,
   } = useCustomSearchParams("PRIVACY_STATUS", ["TAG", "SORT"]);
 
-  // useEffect(() => {
-  //   const fetchTags = async () => {
-  //       try {
-  //         const response = await fetch(`/api/tags?limit=${5}`);
-    
-  //         if (!response.ok) {
-  //           throw new Error("Fetch failed");
-  //         }
-    
-  //         const responseData = (await response.json()) as _ITrendingTags[];
-  //         setTagsData(responseData);
-  //       } catch (err) {
-  //         console.log(err);
-  //         setTagsData([]);
-  //       } finally {
-          
-  //       }
-  //     };
-  //   fetchTags();
-    
-  // }, [])
-
   const tagValue = paramValues.TAG;
   const sortValue = paramValues.SORT;
+
   return (
     <DropdownMenu
       trigger={<IconAdjustmentsHorizontal className="w-6 h-6 cursor-pointer" />}
       style={filterStyles}
       className={cn(
-        "dark:bg-neutral-800 bg-white ring-0 w-86  lg:w-[36rem] rounded-md px-1",
+        "dark:bg-neutral-800 bg-white ring-0 w-[18rem] lg:w-[36rem] rounded-md px-1",
         className
       )}
     >
       {(onClose) => (
         <DropdownMenuContent>
-          <DropdownMenuLabel>
-            <Typography variant="h5">Filter by Privacy</Typography>
-          </DropdownMenuLabel>
-          {feedsOptions.map((option) => (
-            <DropdownMenuItem
-              key={option.value}
-              onClick={() => {
-                handleSetParams(value !== option.value, option.value);
-                onClose();
-              }}
-              className={`${value === option.value ? "bg-gray-200" : ""}`}
-            >
-              <Typography variant="span">{option.label}</Typography>
-            </DropdownMenuItem>
-          ))}
+          {/* Privacy Filter Section */}
+          <DropdownMenuCollapsible
+            trigger={
+              <DropdownMenuLabel>
+                <Typography variant="h5">Filter by Privacy</Typography>
+              </DropdownMenuLabel>
+            }
+            headerClassName="bg-gray-100 dark:bg-neutral-800"
+            contentClassName="bg-white dark:bg-neutral-900"
+          >
+            {feedsOptions.map((option) => (
+              <DropdownMenuItem
+                key={option.value}
+                onClick={() => {
+                  handleSetParams(value !== option.value, option.value);
+                  onClose();
+                }}
+                className={`${value === option.value ? "bg-gray-200" : ""}`}
+              >
+                <Typography variant="span">{option.label}</Typography>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuCollapsible>
 
           <DropdownMenuSeparator />
-          <DropdownMenuLabel className="mt-2">
-            <Typography variant="h5">Filter by Tag</Typography>
-          </DropdownMenuLabel>
-          {tags.map((tag) => (
-            <DropdownMenuItem
-              key={tag._id}
-              onClick={() => {
-                handleSetParams(tagValue !== tag.name, tag.name, "TAG");
-                onClose();
-              }}
-              className={`${tagValue === tag.name ? "bg-gray-200" : ""}`}
-            >
-              <Typography variant="span">#{tag.name}</Typography>
-            </DropdownMenuItem>
-          ))}
+
+          {/* Tag Filter Section */}
+          <DropdownMenuCollapsible
+            trigger={
+              <DropdownMenuLabel>
+                <Typography variant="h5">Filter by Tag</Typography>
+              </DropdownMenuLabel>
+            }
+            headerClassName="bg-gray-100 dark:bg-neutral-800"
+            contentClassName="bg-white dark:bg-neutral-900"
+          >
+            {tags.map((tag) => (
+              <DropdownMenuItem
+                key={tag._id}
+                onClick={() => {
+                  handleSetParams(tagValue !== tag.name, tag.name, "TAG");
+                  onClose();
+                }}
+                className={`${tagValue === tag.name ? "bg-gray-200" : ""}`}
+              >
+                <Typography variant="span">#{tag.name}</Typography>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuCollapsible>
 
           <DropdownMenuSeparator />
-          <DropdownMenuLabel>
-            <Typography variant="h5">Sort By</Typography>
-          </DropdownMenuLabel>
-          {sortOptions.map((option) => (
-            <DropdownMenuItem
-              key={option.value}
-              onClick={() => {
-                handleSetParams(
-                  sortValue !== option.value,
-                  option.value,
-                  "SORT"
-                );
-                onClose();
-              }}
-              className={`${sortValue === option.value ? "bg-gray-200" : ""}`}
-            >
-              <Typography variant="span">{option.label}</Typography>
-            </DropdownMenuItem>
-          ))}
+
+          {/* Sort Section */}
+          <DropdownMenuCollapsible
+            trigger={
+              <DropdownMenuLabel>
+                <Typography variant="h5">Sort By</Typography>
+              </DropdownMenuLabel>
+            }
+            headerClassName="bg-gray-100 dark:bg-neutral-800"
+            contentClassName="bg-white dark:bg-neutral-900"
+          >
+            {sortOptions.map((option) => (
+              <DropdownMenuItem
+                key={option.value}
+                onClick={() => {
+                  handleSetParams(
+                    sortValue !== option.value,
+                    option.value,
+                    "SORT"
+                  );
+                  onClose();
+                }}
+                className={`${sortValue === option.value ? "bg-gray-200" : ""}`}
+              >
+                <Typography variant="span">{option.label}</Typography>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuCollapsible>
         </DropdownMenuContent>
       )}
     </DropdownMenu>
